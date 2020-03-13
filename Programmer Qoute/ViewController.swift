a2cc4df7748539ed0f013bec55ae80e663afa9ba
nickbiever <nickbiever01@gmail.com>
2020-03-13 11:48:05 +0100

    Initial Commit

---
+++ Programmer Qoute/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Programmer Qoute
+//
+//  Created by Nick Biever on 13/03/2020.
+//  Copyright © 2020 Nick Biever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
